1210093f371ded3281ef84f1c2eaeca61fea677a
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-12-01 11:46:52 +0900

    tip js 완료

--- js/tip.js
+++ js/tip.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    
+    $('.check_group').click(function(e){
+        let num=$('.check_group').index(this)
+        let img_str="#check_image"+(num+1);
+        // let label_str="#check_label"+(num+1);
+        $('.check_group img').attr('src', '../images/check_before.png');
+        // $('.check_group label').css('background-color', 'transparent');
+        $(img_str).attr('src', '../images/check_after.png');
+        // $(label_str).css('background-color', '#FEF2CE');
+    })
+    $('#play_btn').hover(function(){
+
+    })
+    $('#play_btn').click(function(){
+        //의사결정나무
+    })
 })
