a64430bc0b479a35b88ee0a9e453254cc47c95fa
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-12-01 21:46:48 +0900

    장소 입력시 거리구하기 밑작업

--- js/tip.js
+++ js/tip.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
+    var select_num=2;
     $('.check_group').click(function(e){
-        let num=$('.check_group').index(this)
-        let img_str="#check_image"+(num+1);
-        // let label_str="#check_label"+(num+1);
+        let num=$('.check_group').index(this);
+        select_num=num+1;
+        let img_str="#check_image"+select_num;
         $('.check_group img').attr('src', '../images/check_before.png');
-        // $('.check_group label').css('background-color', 'transparent');
         $(img_str).attr('src', '../images/check_after.png');
-        // $(label_str).css('background-color', '#FEF2CE');
     })
     $('#play_btn').click(function(){
-        //영수증 바꾸기
+        console.log("sdf")
+        let starting_point=$('#start_place_text').attr('value');
+        let destination=$('#end_place_text').attr('value');
+        let desirability=$('#check_label'+select_num).attr('value');
+
+       // $('#rank_text1').text(starting_point);
+       // $('#rank_text2').text(destination);
+       // $('#rank_text3').text(desirability);
+
+        let str="https://www.google.co.kr/maps/dir/"+starting_point+", New York/"+destination+", New York";
+        window.open(str);
+        
+        //의사결정나무
     })
 })
